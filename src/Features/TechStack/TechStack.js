3f7dfc2c1e96ef01fc384e1ac5b1573554728d7f
Sagar585 <sagar.nb@somaiya.edu>
2024-03-28 21:03:14 +0530

    Done with tech stack

--- src/Features/TechStack/TechStack.js
+++ src/Features/TechStack/TechStack.js
@@ -9,38 +9,62 @@ import MongoDb from "../../assets/MongoDb.png";
 import Tailwindcss from "../../assets/Tailwindcss.png";
 import DjangoImage from "../../assets/Django.png";
 
-
 const techStackItems = [
-  { name: "Html", image: HtmlImage },
-  { name: "Css", image: CssImage },
-  { name: "TailwindCss", image: Tailwindcss },
-  { name: "MongoDb", image: MongoDb },
-  { name: "React", image: ReactImage },
-  { name: "Node & Express.js", image: NodeImage },
-  { name: "BootStrap", image: BootsrapImage },
-  { name: "Django", image: DjangoImage },
+  { name: "Html", image: HtmlImage, typeof: "Frontend" },
+  { name: "Css", image: CssImage, typeof: "Frontend" },
+  { name: "TailwindCss", image: Tailwindcss, typeof: "Frontend" },
+  { name: "MongoDb", image: MongoDb, typeof: "Backend" },
+  { name: "React", image: ReactImage, typeof: "Frontend" },
+  { name: "Node & Express.js", image: NodeImage, typeof: "Backend" },
+  { name: "BootStrap", image: BootsrapImage, typeof: "Frontend" },
+  { name: "Django", image: DjangoImage, typeof: "Backend" },
 ];
 
 export default function TechStack() {
+  const frontendItems = techStackItems.filter(
+    (item) => item.typeof === "Frontend"
+  );
+  const backendItems = techStackItems.filter(
+    (item) => item.typeof === "Backend"
+  );
+
   return (
     <>
       <div className="DisplayFlex TechSec">
         <div className="Skills DisplayFlex">
           <h1>Skills </h1>
           <span>
-            Here are My Some Skills I Have Been Worked On Since My Past 1 Year
+            Here are My Some Skills I Have Been Worked On Since My Past 1.5 Year
           </span>
         </div>
+        <div className="TechCardContainer">
+
         <div className="tech-stack-container">
-          {techStackItems &&
-            techStackItems.map((item, index) => (
-              <div className="tech-stack-item" key={index}>
-                <img src={item.image} alt={item.name} />
-                <p>{item.name}</p>
+          <div className="cards">
+            <h2 className="Subheading">FrontEnd</h2>
+            <div className="TechContainer">
+              {frontendItems.map((item, index) => (
+                <div className="tech-stack-item" key={index}>
+                  <img src={item.image} alt={item.name} />
+                  <p>{item.name}</p>
+                </div>
+              ))}
+            </div>
+            </div>
+            <div className="cards">
+              <h2 className="Subheading">Backend</h2>
+              <div className="TechContainer">
+                {backendItems.map((item, index) => (
+                  <div className="tech-stack-item" key={index}>
+                    <img src={item.image} alt={item.name} />
+                    <p>{item.name}</p>
+                  </div>
+                ))}
               </div>
-            ))}
+          </div>
         </div>
       </div>
+                </div>
     </>
   );
 }
